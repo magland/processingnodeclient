fb410f1559e18041219d0c9a59b28aa07ff9a02d
Jeremy Magland <jeremy.magland@gmail.com>
2014-05-01 15:31:20 +0000

    add install_qt4.sh

--- INSTALL/install.sh
+++ INSTALL/install.sh
@@ -1,6 +1,7 @@
 sudo sh install_nodejs.sh
 sudo sh install_mongodb.sh
 sudo sh install_octave.sh
+sudo sh install_qt4.sh
 sh install_node_packages.sh
 sudo sh install_misc.sh
 
---
+++ INSTALL/install_qt4.sh
@@ -0,0 +1,1 @@
+apt-get install qt4-dev-tools
